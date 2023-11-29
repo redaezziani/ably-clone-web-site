3acdb004ed28b5cb389481ea7a1041ef1e5dc074
redaezziani <2003100500167@ofppt-edu.ma>
2023-11-29 16:24:35 +0000

    First commit 🦄

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <div className="flex w-full min-h-screen flex-col gap-3 justify-start items-center">
+      <h1
+      className="text-3xl font-bold text-gray-800"
+      >
+        this is the ably clone app
+      </h1>
+    </div>
+  )
+}
+
+export default App
